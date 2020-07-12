23f5df65164dc63126ec65ac4a5989ea1d160985
Victor Coda <victor.coda@gmail.com>
2020-07-12 17:11:14 +0300

    Implemented optional Cylinder's top and bottom caps

--- include/cylinder.h
+++ include/cylinder.h
@@ -3,24 +3,32 @@
 
 namespace quadric
 {
+    class SinCosTable;
+
     class Cylinder : public Quadric
     {
     public:
-        explicit Cylinder(float topRadius, float bottomRadius, float length, uint16_t sides, uint16_t rings,
+        explicit Cylinder(float topRadius, float bottomRadius, float length, uint16_t sides, uint16_t rings, bool capped,
             bool textureMirrorRepeat, std::shared_ptr<magma::CommandBuffer> cmdBuffer);
         float getTopRadius() const noexcept { return topRadius; }
         float getBottomRadius() const noexcept { return bottomRadius; }
         uint16_t getNumSides() const noexcept { return sides; }
         uint16_t getNumRings() const noexcept { return rings; }
+        bool isCapped() const noexcept { return capped; }
 
     private:
+        void pole(float y, Vertex& v) const noexcept;
+        void cap(const SinCosTable& phi, float radius, float y,
+            Vertex *verts, Face *faces, uint16_t firstFaceIndex) const noexcept;
         float calcSCoord(uint16_t side, bool mirror) const noexcept;
         float calcTCoord(uint16_t ring) const noexcept;
         uint16_t calcVertexIndex(uint16_t ring, uint16_t side) const noexcept;
+        uint16_t calcPoleVertexIndex(bool south) const noexcept;
 
         const float topRadius;
         const float bottomRadius;
         const uint16_t sides;
         const uint16_t rings;
+        const bool capped;
     };
 }
--- src/cylinder.cpp
+++ src/cylinder.cpp
@@ -7,13 +7,16 @@ using namespace quadric;
 using namespace rapid;
 using namespace rapid::constants;
 
-Cylinder::Cylinder(float topRadius, float bottomRadius, float length, uint16_t sides, uint16_t rings,
+Cylinder::Cylinder(float topRadius, float bottomRadius, float length, uint16_t sides, uint16_t rings, bool capped,
     bool textureMirrorRepeat, std::shared_ptr<magma::CommandBuffer> cmdBuffer):
-    Quadric((sides + 1) * (rings + 1), sides * rings * 2, cmdBuffer->getDevice()),
+    Quadric((sides + 1) * (rings + 1) + (capped ? (sides + 2) * 2 : 0),
+        sides * rings * 2 + (capped ? sides * 2 : 0),
+        cmdBuffer->getDevice()),
     topRadius(topRadius),
     bottomRadius(bottomRadius),
     sides(sides),
-    rings(rings)
+    rings(rings),
+    capped(capped)
 {
     const float radiusDelta = topRadius - bottomRadius;
     const float radiusStep = radiusDelta/rings;
@@ -29,8 +32,8 @@ Cylinder::Cylinder(float topRadius, float bottomRadius, float length, uint16_t s
         ny = 0.f;
 #endif
     // phi - angle on xz plane
-    const float phiStep = rapid::constants::twoPi/sides;
-    SinCosTable phi(rapid::constants::halfPi, phiStep, sides + 1);
+    const float phiStep = twoPi/sides;
+    SinCosTable phi(halfPi, phiStep, sides + 1);
     // Calculate rings
     Vertex *verts = vertices->getMemory()->map<Vertex>();
     for (uint16_t ring = 0; ring < rings + 1; ++ring)
@@ -71,7 +74,57 @@ Cylinder::Cylinder(float topRadius, float bottomRadius, float length, uint16_t s
             face1.v[2] = calcVertexIndex(ring + 1, side);
         }
     }
+    if (capped)
+    {
+        uint16_t firstFaceIndex = sides * rings * 2;
+        cap(phi, topRadius, length/2.f, verts, faces, firstFaceIndex);
+        firstFaceIndex += sides;
+        cap(phi, bottomRadius, -length/2.f, verts, faces, firstFaceIndex);
+    }
     upload(std::move(cmdBuffer));
+}
+
+void Cylinder::pole(float y, Vertex& v) const noexcept
+{
+    v.pos.x = 0.f;
+    v.pos.y = y;
+    v.pos.z = 0.f;
+    v.normal = {0.f, sign(y), 0.f};
+    v.tex.s = .5f;
+    v.tex.t = .5f;
+}
+
+void Cylinder::cap(const SinCosTable& phi, float radius, float y,
+    Vertex *verts, Face *faces, uint16_t firstFaceIndex) const noexcept
+{
+    const bool bottom = (y < 0.f);
+    const uint16_t poleIndex = calcPoleVertexIndex(bottom);
+    pole(y, verts[poleIndex]);
+    const float ny = sign(y);
+    // Calculate vertices
+    uint16_t vertexIndex = poleIndex + 1;
+    for (uint16_t side = 0; side < sides + 1; ++side)
+    {
+        Vertex& v = verts[vertexIndex++];
+        v.pos.x = radius * phi.cos(side);
+        v.pos.z = radius * phi.sin(side);
+        v.pos.y = y;
+        v.normal = {0.f, ny, 0.f};
+        v.tex.s = phi.cos(side) * .5f + .5f;
+        v.tex.t = phi.sin(side) * .5f + .5f;
+        if (bottom) // Flip T coord
+            v.tex.t = 1.f - v.tex.t;
+    }
+    // Build indices
+    for (uint16_t side = 0; side < sides; ++side)
+    {
+        Face& face = faces[firstFaceIndex + side];
+        face.v[0] = poleIndex + side + 1;
+        face.v[1] = poleIndex;
+        face.v[2] = poleIndex + side + 2;
+        if (bottom) // Swap face indices
+            std::swap(face.v[0], face.v[1]);
+    }
 }
 
 inline float Cylinder::calcSCoord(uint16_t side, bool mirror) const noexcept
@@ -91,4 +144,12 @@ inline uint16_t Cylinder::calcVertexIndex(uint16_t ring, uint16_t side) const no
     const uint32_t index = ring * (sides + 1) + side;
     assert(index <= std::numeric_limits<uint16_t>::max());
     return (uint16_t)index;
+}
+
+uint16_t Cylinder::calcPoleVertexIndex(bool south) const noexcept
+{
+    uint16_t index = (sides + 1) * (rings + 1);
+    if (south) index += (sides + 1) + 1; // top cap
+    assert(index <= std::numeric_limits<uint16_t>::max());
+    return index;
 }
